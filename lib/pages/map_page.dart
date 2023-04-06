aa7f0269ff53863e2168732f5d47225353e8f328
T-Pirozzini <tpirozzini@gmail.com>
2023-04-06 13:09:42 -0700

    style search bar and my location widgets

--- lib/pages/map_page.dart
+++ lib/pages/map_page.dart
@@ -38,65 +38,6 @@ class MapPageState extends State<MapPage> {
     ),
   );
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-        children: [
-          Row(
-            children: [
-              Expanded(
-                child: TextFormField(
-                  controller: _searchController,
-                  textCapitalization: TextCapitalization.words,
-                  decoration: const InputDecoration(hintText: 'Search by City'),
-                  onChanged: (value) {},
-                ),
-              ),
-              IconButton(
-                onPressed: () async {
-                  var place =
-                      await LocationService().getPlace(_searchController.text);
-                  _goToPlace(place);
-                },
-                icon: const Icon(Icons.search),
-              ),
-            ],
-          ),
-          Expanded(
-            child: GoogleMap(
-              mapType: MapType.normal,
-              markers: {
-                _kGooglePlexMarker,
-              },
-              initialCameraPosition: _kGooglePlex,
-              onMapCreated: (GoogleMapController controller) {
-                _controller.complete(controller);
-                controller.setMapStyle(mapstyle);
-              },
-              padding: const EdgeInsets.only(bottom: 60),
-            ),
-          ),
-        ],
-      ),
-      floatingActionButton: Stack(
-        children: [
-          Positioned(
-            top: 80.0,
-            right: 5.0,
-            child: FloatingActionButton(
-              onPressed: _determinePosition,
-              shape: const RoundedRectangleBorder(),
-              mini: true,
-              backgroundColor: Colors.grey.shade800,
-              child: const Icon(Icons.my_location),
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-
   Future<void> _goToPlace(Map<String, dynamic> place) async {
     final double lat = place['geometry']['location']['lat'];
     final double lng = place['geometry']['location']['lng'];
@@ -143,5 +84,84 @@ class MapPageState extends State<MapPage> {
       ),
     );
     return position;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Row(
+            children: [
+              Expanded(
+                child: TextFormField(
+                  controller: _searchController,
+                  textCapitalization: TextCapitalization.words,
+                  decoration: InputDecoration(
+                    contentPadding: const EdgeInsets.symmetric(
+                      vertical: 12.0,
+                      horizontal: 16.0,
+                    ),
+                    hintText: 'Search by City',
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.zero,
+                      borderSide: BorderSide(
+                        color: Colors.grey.shade600,
+                        width: 2.0,
+                      ),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.zero,
+                      borderSide: BorderSide(
+                        color: Colors.grey.shade800,
+                        width: 2.0,
+                      ),
+                    ),
+                    suffixIcon: IconButton(
+                      onPressed: () async {
+                        var place = await LocationService()
+                            .getPlace(_searchController.text);
+                        _goToPlace(place);
+                      },
+                      icon: const Icon(Icons.search),
+                    ),
+                  ),
+                  onChanged: (value) {},
+                ),
+              ),
+            ],
+          ),
+          Expanded(
+            child: GoogleMap(
+              mapType: MapType.normal,
+              markers: {
+                _kGooglePlexMarker,
+              },
+              initialCameraPosition: _kGooglePlex,
+              onMapCreated: (GoogleMapController controller) {
+                _controller.complete(controller);
+                controller.setMapStyle(mapstyle);
+              },
+              padding: const EdgeInsets.only(bottom: 60),
+            ),
+          ),
+        ],
+      ),
+      floatingActionButton: Stack(
+        children: [
+          Positioned(
+            top: 80.0,
+            right: 5.0,
+            child: FloatingActionButton(
+              onPressed: _determinePosition,
+              shape: const RoundedRectangleBorder(),
+              mini: true,
+              backgroundColor: Colors.grey.shade800,
+              child: const Icon(Icons.my_location),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
