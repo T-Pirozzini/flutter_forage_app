1113ee6a3545b4d1e93b8f871786d4491011c517
T-Pirozzini <tpirozzini@gmail.com>
2023-04-06 19:11:02 -0700

    prep for flutter upgrade

---
+++ lib/pages/explore_page.dart
@@ -0,0 +1,156 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:flutter_forager_app/components/map_style.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:geolocator/geolocator.dart';
+import '../components/search_field.dart';
+
+class ExplorePage extends StatefulWidget {
+  const ExplorePage({Key? key}) : super(key: key);
+
+  @override
+  State<ExplorePage> createState() => ExplorePageState();
+}
+
+class ExplorePageState extends State<ExplorePage> {
+  final Completer<GoogleMapController> _controller =
+      Completer<GoogleMapController>();
+
+  CameraPosition _initialCameraPosition = const CameraPosition(
+    target: LatLng(37.42796133580664, -122.085749655962),
+    zoom: 14,
+  );
+
+  @override
+  void initState() {
+    super.initState();
+    _determinePosition();
+  }
+
+  // Future<void> _getCurrentLocation() async {
+  //   final position = await Geolocator.getCurrentPosition(
+  //       desiredAccuracy: LocationAccuracy.high);
+  //   setState(() {
+  //     _initialCameraPosition = CameraPosition(
+  //       target: LatLng(position.latitude, position.longitude),
+  //       zoom: 14.0,
+  //     );
+  //   });
+  // }
+
+  Future<void> _goToPlace(Map<String, dynamic> place) async {
+    final double lat = place['geometry']['location']['lat'];
+    final double lng = place['geometry']['location']['lng'];
+    final GoogleMapController controller = await _controller.future;
+    controller.animateCamera(
+      CameraUpdate.newCameraPosition(
+        CameraPosition(target: LatLng(lat, lng), zoom: 14),
+      ),
+    );
+  }
+
+  bool _isPressed = false;
+  Future<Position> _determinePosition() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    // Test if location services are enabled.
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      return Future.error('Location services are disabled.');
+    }
+
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        return Future.error('Location permissions are denied');
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      return Future.error(
+          'Location permissions are permanently denied, we cannot request permissions.');
+    }
+
+    final position = await Geolocator.getCurrentPosition();
+    if (mounted) {
+      setState(
+        () {
+          _isPressed = false;
+          _initialCameraPosition = CameraPosition(
+            target: LatLng(position.latitude, position.longitude),
+            zoom: 14.0,
+          );
+        },
+      );
+    }
+
+    final GoogleMapController controller = await _controller.future;
+    controller.animateCamera(
+      CameraUpdate.newCameraPosition(
+        CameraPosition(
+          target: LatLng(position.latitude, position.longitude),
+          zoom: 14,
+        ),
+      ),
+    );
+    return position;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Row(
+            children: [
+              Expanded(
+                child: SearchField(
+                  onPlaceSelected: _goToPlace,
+                ),
+              ),
+            ],
+          ),
+          Expanded(
+            child: GoogleMap(
+              mapType: MapType.normal,
+              markers: {},
+              initialCameraPosition: _initialCameraPosition,
+              onMapCreated: (GoogleMapController controller) {
+                _controller.complete(controller);
+                controller.setMapStyle(mapstyle);
+              },
+              padding: const EdgeInsets.only(bottom: 60),
+            ),
+          ),
+        ],
+      ),
+      floatingActionButton: Stack(
+        children: [
+          Positioned(
+            top: 80.0,
+            right: 5.0,
+            child: FloatingActionButton(
+              onPressed: () {
+                setState(
+                  () {
+                    _isPressed = true;
+                  },
+                );
+                _determinePosition();
+              },
+              shape: const RoundedRectangleBorder(),
+              mini: true,
+              backgroundColor: Colors.grey.shade800,
+              child: Icon(
+                Icons.my_location,
+                color: _isPressed ? Colors.deepOrange.shade300 : Colors.white,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
