3638edb0c18db8d9cc0a53ad61c8c46a7bce2366
T-Pirozzini <tpirozzini@gmail.com>
2023-07-05 13:04:42 -0700

    display in ListView draft post information

--- lib/pages/community_page.dart
+++ lib/pages/community_page.dart
@@ -1,10 +1,11 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class CommunityPage extends StatefulWidget {
-  
-
-  const CommunityPage({super.key, });
+  const CommunityPage({
+    super.key,
+  });
 
   @override
   State<CommunityPage> createState() => _CommunityPageState();
@@ -14,16 +15,41 @@ class _CommunityPageState extends State<CommunityPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('COMMUNITY'),
-        titleTextStyle:
-            GoogleFonts.philosopher(fontSize: 24, fontWeight: FontWeight.bold),
-        centerTitle: true,
-        backgroundColor: Colors.deepOrange.shade400,
-      ),
-      body: const Center(
-        child: Text('Community Page'),
-      ),
-    );
+        appBar: AppBar(
+          title: const Text('COMMUNITY'),
+          titleTextStyle: GoogleFonts.philosopher(
+              fontSize: 24, fontWeight: FontWeight.bold),
+          centerTitle: true,
+          backgroundColor: Colors.deepOrange.shade400,
+        ),
+        body: Column(
+          children: [
+            Expanded(
+              child: StreamBuilder<QuerySnapshot>(
+                stream:
+                    FirebaseFirestore.instance.collection('Posts').snapshots(),
+                builder: (context, snapshot) {
+                  if (snapshot.hasData) {
+                    return ListView.builder(
+                        itemCount: snapshot.data!.docs.length,
+                        itemBuilder: (context, index) {
+                          DocumentSnapshot ds = snapshot.data!.docs[index];
+                          return Card(
+                            child: ListTile(
+                              title: Text(ds['name']),
+                              subtitle: Text(ds['type']),
+                            ),
+                          );
+                        });
+                  } else {
+                    return const Center(
+                      child: CircularProgressIndicator(),
+                    );
+                  }
+                },
+              ),
+            ),
+          ],
+        ));
   }
 }
