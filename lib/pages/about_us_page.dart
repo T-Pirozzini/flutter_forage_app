a8ece79c54f2cc6960ab101f6dd79331bcd9beea
au-richard <richard.au04@gmail.com>
2023-07-12 15:10:44 -0700

    Updating bio for Richard. Adding bio background image.

--- lib/pages/about_us_page.dart
+++ lib/pages/about_us_page.dart
@@ -188,7 +188,7 @@ class _AboutUsPageState extends State<AboutUsPage> {
               child: Stack(
                 children: [
                   Image.asset(
-                    'lib/assets/images/richard_about.png',
+                    'lib/assets/images/richard_about2.jpg',
                   ),
                   Container(
                     color: Colors.black.withOpacity(0.5),
@@ -202,16 +202,23 @@ class _AboutUsPageState extends State<AboutUsPage> {
                               fontWeight: FontWeight.bold,
                               color: Colors.white),
                         ),
-                        const Text(
+                        const Padding(
+                          padding: EdgeInsets.only(top: 8.0),
+                          child: Text(
                             'Mobile Developer, Full-Stack Software Developer',
                             style: TextStyle(
                                 color: Colors.white,
                                 fontSize: 16,
-                                fontWeight: FontWeight.bold)),
-                        const Text(
-                          'Whether I\'m on the basketball court, coding at my desk, or cooking in the kitchen, I\'m always looking to experiment with new ideas.I love blending two entirely different industries together and testing what the outcome would be for an idea. By learning software development and combining my  previous knowledge in social sciences and psychology, I aim to bridge the gap between individuals and mental health clinics to be more easily accessible and more well-known publicly.',
-                          style: TextStyle(color: Colors.white, fontSize: 14),
-                          textAlign: TextAlign.center,
+                                fontWeight: FontWeight.bold),
+                          ),
+                        ),
+                        const Padding(
+                          padding: EdgeInsets.all(12.0),
+                          child: Text(
+                            '\n Whether I\'m on the basketball court, coding at my desk, or cooking in the kitchen, I\'m always looking to experiment with new ideas.I love blending two entirely different industries together and testing what the outcome would be for an idea. \n\nBy learning software development and combining my  previous knowledge in social sciences and psychology, I aim to bridge the gap between individuals and mental health clinics to be more easily accessible and more well-known publicly.',
+                            style: TextStyle(color: Colors.white, fontSize: 14),
+                            textAlign: TextAlign.center,
+                          ),
                         ),
                         const SizedBox(height: 16),
                         const SizedBox(height: 5),
@@ -223,30 +230,33 @@ class _AboutUsPageState extends State<AboutUsPage> {
                             textAlign: TextAlign.center,
                           ),
                         ),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            ElevatedButton.icon(
-                              onPressed: _launchWebsiteRichard,
-                              icon: const FaIcon(FontAwesomeIcons
-                                  .person), // Icon for Personal Website
-                              label: const Text('Portfolio'),
-                            ),
-                            const SizedBox(width: 16),
-                            ElevatedButton.icon(
-                              onPressed: _launchLinkedInRichard,
-                              icon: const FaIcon(FontAwesomeIcons
-                                  .linkedin), // Icon for LinkedIn
-                              label: const Text('LinkedIn'),
-                            ),
-                            const SizedBox(width: 16),
-                            ElevatedButton.icon(
-                              onPressed: _launchGitHubRichard,
-                              icon: const FaIcon(
-                                  FontAwesomeIcons.github), // Icon for GitHub
-                              label: const Text('GitHub'),
-                            ),
-                          ],
+                        Padding(
+                          padding: EdgeInsets.only(top: 40.0),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              ElevatedButton.icon(
+                                onPressed: _launchWebsiteRichard,
+                                icon: const FaIcon(FontAwesomeIcons
+                                    .person), // Icon for Personal Website
+                                label: const Text('Portfolio'),
+                              ),
+                              const SizedBox(width: 16),
+                              ElevatedButton.icon(
+                                onPressed: _launchLinkedInRichard,
+                                icon: const FaIcon(FontAwesomeIcons
+                                    .linkedin), // Icon for LinkedIn
+                                label: const Text('LinkedIn'),
+                              ),
+                              const SizedBox(width: 16),
+                              ElevatedButton.icon(
+                                onPressed: _launchGitHubRichard,
+                                icon: const FaIcon(
+                                    FontAwesomeIcons.github), // Icon for GitHub
+                                label: const Text('GitHub'),
+                              ),
+                            ],
+                          ),
                         ),
                       ],
                     ),
