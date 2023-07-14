c2624bcb280806a2a32271de73305e876a504a91
T-Pirozzini <tpirozzini@gmail.com>
2023-07-14 14:15:38 -0700

    add confirmation dialog before permanently deleting a community post

--- lib/pages/community_page.dart
+++ lib/pages/community_page.dart
@@ -101,15 +101,14 @@ class _CommunityPageState extends State<CommunityPage> {
   }
 
   void removeMarkerInfo(
-    String markerName,
-    String markerDescription,
-    String markerType,
-    String? markerImageUrl,
-    double markerLatitude,
-    double markerLongitude,
-    Timestamp timestamp,
-    String markerOwner
-  ) async {
+      String markerName,
+      String markerDescription,
+      String markerType,
+      String? markerImageUrl,
+      double markerLatitude,
+      double markerLongitude,
+      Timestamp timestamp,
+      String markerOwner) async {
     final userMarkersRef = FirebaseFirestore.instance
         .collection('Users')
         .doc(currentUser.email)
@@ -239,7 +238,7 @@ class _CommunityPageState extends State<CommunityPage> {
                               post['longitude'],
                               Timestamp.fromDate(
                                   DateTime.parse(post['postTimestamp'])),
-                                  post['user'],
+                              post['user'],
                             );
                           }
                         } else {
@@ -264,7 +263,7 @@ class _CommunityPageState extends State<CommunityPage> {
                               post['longitude'],
                               Timestamp.fromDate(
                                   DateTime.parse(post['postTimestamp'])),
-                                  post['user'],
+                              post['user'],
                             );
                           }
                         }
@@ -275,13 +274,45 @@ class _CommunityPageState extends State<CommunityPage> {
                       void deletePost() async {
                         final currentUserEmail = currentUser.email;
                         if (post['user'] == currentUserEmail) {
-                          await postsCollection.doc(post.id).delete();
+                          showDialog(
+                            context: context,
+                            builder: (BuildContext context) {
+                              return AlertDialog(
+                                title: const Text('Confirmation'),
+                                content: const Text(
+                                    'Are you sure you want to delete this post?'),
+                                actions: [
+                                  TextButton(
+                                    child: const Text('Cancel'),
+                                    onPressed: () {
+                                      Navigator.of(context).pop();
+                                    },
+                                  ),
+                                  TextButton(
+                                    child: const Text('Delete'),
+                                    onPressed: () async {
+                                      await postsCollection
+                                          .doc(post.id)
+                                          .delete();
+                                      Navigator.of(context)
+                                          .pop(); // Close the confirmation dialog
+                                      ScaffoldMessenger.of(context)
+                                          .showSnackBar(
+                                        const SnackBar(
+                                          content: Text('Post deleted.'),
+                                        ),
+                                      );
+                                    },
+                                  ),
+                                ],
+                              );
+                            },
+                          );
                         } else {
                           ScaffoldMessenger.of(context).showSnackBar(
                             const SnackBar(
-                              content: Text(
-                                'You can only delete your own posts.',
-                              ),
+                              content:
+                                  Text('You can only delete your own posts.'),
                             ),
                           );
                         }
