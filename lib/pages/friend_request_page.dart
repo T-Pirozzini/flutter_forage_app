b49edabcf7a0ca0d60e5704c3738ad6e500755d7
T-Pirozzini <tpirozzini@gmail.com>
2023-11-27 17:25:21 -0800

    add friends controller and update friends page method of retrieving and displaying data

--- lib/pages/friend_request_page.dart
+++ lib/pages/friend_request_page.dart
@@ -13,6 +13,105 @@ class FriendRequestPage extends StatefulWidget {
 class _FriendRequestPageState extends State<FriendRequestPage> {
   // current user
   final currentUser = FirebaseAuth.instance.currentUser!;
+  List<Map<String, dynamic>> _searchResults = [];
+  late final TextEditingController _searchController;
+
+  @override
+  void initState() {
+    _searchController = TextEditingController();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _searchController.dispose();
+    super.dispose();
+  }
+
+  // search for users
+  Future<void> _searchUsers(String searchTerm) async {
+    final currentUserEmail = FirebaseAuth.instance.currentUser!.email!;
+    if (searchTerm == currentUserEmail) {
+      // If the search term is the same as the current user's email, return without performing the search
+      setState(() {
+        _searchResults = [];
+      });
+      return;
+    }
+
+    final usersCollection = FirebaseFirestore.instance.collection('Users');
+    final querySnapshot =
+        await usersCollection.where('email', isEqualTo: searchTerm).get();
+
+    final currentUserData = await FirebaseFirestore.instance
+        .collection('Users')
+        .doc(currentUserEmail)
+        .get()
+        .then((snapshot) => snapshot.data());
+
+    final List<dynamic> currentUserFriends =
+        currentUserData?['friends'] ?? <dynamic>[];
+    final List<dynamic> currentUserSentFriendRequests =
+        currentUserData?['sentFriendRequests'] ?? <dynamic>[];
+
+    setState(() {
+      _searchResults = querySnapshot.docs.map((doc) {
+        final userData = doc.data();
+        final userEmail = userData['email'] ?? '';
+
+        // Check if the searched user is already a friend
+        final isFriend = currentUserFriends.any((friend) =>
+            friend['email'] != null && friend['email'] == userEmail);
+
+        // Check if the friend request is already sent
+        final isFriendRequestSent = currentUserSentFriendRequests.any(
+            (request) =>
+                request['email'] != null && request['email'] == userEmail);
+
+        // Return modified user data with isFriend and isFriendRequestSent flags
+        return {
+          ...userData,
+          'isFriend': isFriend,
+          'isFriendRequestSent': isFriendRequestSent,
+        };
+      }).toList();
+    });
+  }
+
+  Future<void> _sendFriendRequest(String userEmail) async {
+    final currentUserEmail = FirebaseAuth.instance.currentUser!.email!;
+    final usersCollection = FirebaseFirestore.instance.collection('Users');
+    final timeSent = Timestamp.now();
+
+    // Check if the friend request is already sent or pending
+    final userDoc = await usersCollection.doc(userEmail).get();
+    final friendRequests = userDoc.data()?['friendRequests'] ?? <dynamic>[];
+    final isFriendRequestSent = friendRequests.any((request) =>
+        request['email'] != null && request['email'] == currentUserEmail);
+
+    if (isFriendRequestSent) {
+      // Show snackbar if the friend request is already pending
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+            content: Text('Waiting for $userEmail to accept your request')),
+      );
+      return;
+    }
+
+    // Add the friend request to the selected user's list
+    await usersCollection.doc(userEmail).update({
+      'friendRequests': FieldValue.arrayUnion([
+        {'email': currentUserEmail, 'timestamp': timeSent}
+      ]),
+    });
+
+    // Add the selected user to the current user's sent friend requests list
+    await usersCollection.doc(currentUserEmail).update({
+      'sentFriendRequests': FieldValue.arrayUnion([
+        {'email': userEmail, 'timestamp': timeSent},
+      ]),
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -27,6 +126,55 @@ class _FriendRequestPageState extends State<FriendRequestPage> {
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: SizedBox(
+              height: 50,
+              child: TextField(
+                controller: _searchController,
+                onChanged: (value) => _searchUsers(value),
+                decoration: const InputDecoration(
+                  hintText: 'Search users...',
+                  contentPadding: EdgeInsets.all(8),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(8)),
+                  ),
+                ),
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 100,
+            child: ListView.builder(
+              itemCount: _searchResults.length,
+              itemBuilder: (context, index) {
+                final user = _searchResults[index];
+                final isFriend = user['isFriend'] ?? false;
+                final isFriendRequestSent =
+                    user['isFriendRequestSent'] ?? false;
+
+                return ListTile(
+                  title: Text(user['username'] ?? ''),
+                  subtitle: Text(user['email'] ?? ''),
+                  trailing: isFriend
+                      ? const Icon(Icons.check,
+                          color: Colors
+                              .green) // Display green checkmark for friends
+                      : isFriendRequestSent
+                          ? const Icon(Icons.pending,
+                              color: Colors
+                                  .deepOrange) // Display pending-related icon for friend requests sent
+                          : const Icon(
+                              Icons.add), // Display plus sign for other users
+                  onTap: () {
+                    if (!isFriend && !isFriendRequestSent) {
+                      _sendFriendRequest(user['email']);
+                    }
+                  },
+                );
+              },
+            ),
+          ),
           Expanded(
             child: StreamBuilder<DocumentSnapshot>(
               stream: FirebaseFirestore.instance
